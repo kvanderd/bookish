c85715d499ffa1303597c3b96d29d49f739ca62d
Alex Edelstein <alex@edelstein.org>
2013-08-18 16:59:06 -0700

    we forgot to git rm these unnedeeded migrations

--- db/migrate/20130815191812_add_name_to_widgets.rb
+++
@@ -1,5 +0,0 @@
-class AddNameToWidgets < ActiveRecord::Migration
-  def change
-    add_column :widgets, :name, :string
-  end
-end
--- db/migrate/20130815213918_add_html_block1_to_widget.rb
+++
@@ -1,5 +0,0 @@
-class AddHtmlBlock1ToWidget < ActiveRecord::Migration
-  def change
-    add_column :widgets, :html_block_1, :text
-  end
-end
